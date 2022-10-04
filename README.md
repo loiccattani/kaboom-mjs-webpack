441912d1dec56493401968686db3a9b8df8e3d03
Loïc Cattani <loic@cattani.co>
2022-10-04 15:46:17 +0200

    kaboom from NPM, dev server and live reload

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
 # kaboom-mjs-webpack
 
-A ready to fork Kaboom project boilerplate with ES6 modules, webpack.
+A Kaboom project boilerplate with ES6 modules and webpack.
 
 Some CSS styles applied to center the canvas on screen both horizontally and vertically at native resolution (will shrink to fit).
 
-## Build for deployment
-
-### Install dev dependencies (webpack)
+## Install dependencies
 
     npm install
 
-### Build
+## Development server and live reload
+
+Serve the project, watch files for change and live reload.
+
+    npm run start
+
+## Build for deployment
 
     npm run build
 
 `dist` will contain the code ready for deployment
-
-## Download or update local Kaboom (for use when network is not available)
-
-A copy of Kaboom 2000.2.9 code and source map is already included in `vendor/`.
-
-To update kaboom files locally, run these commands in the terminal:
-
-    curl -Lo vendor/kaboom.mjs https://unpkg.com/kaboom/dist/kaboom.mjs
-    curl -Lo vendor/kaboom.mjs.map https://unpkg.com/kaboom/dist/kaboom.mjs.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,34 @@
-const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
 
 module.exports = {
-    mode: 'production',
+    // Doc: https://webpack.js.org/configuration/#options
+    mode: 'development',
     entry: './js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/main.js',
-        clean: true,
-        publicPath: '/dist/',
+    devtool: 'source-map',
+    devServer: {
+        // Doc: https://webpack.js.org/configuration/dev-server
+        static: './dist',
     },
     plugins: [
+        // Doc: https://github.com/jantimon/html-webpack-plugin
+        new HtmlWebpackPlugin({ // Generate an HTML index to serve bundles and assets
+            title: 'Kaboom',    // The generated HTML page title
+            hash: true,         // Add a query param hash to files for cache busting
+        }),
+        // Doc: https://github.com/jharris4/html-webpack-tags-plugin
+        new HtmlWebpackTagsPlugin({
+            tags: [
+                'stylesheets/main.css', // Include the copied CSS in the HTML head
+            ],
+        }),
+        // https://github.com/webpack-contrib/copy-webpack-plugin
         new CopyPlugin({
             patterns: [
-                '*.html',
                 './stylesheets/*.css',
                 './images/*',
                 //'./sounds/*',
@@ -24,6 +36,10 @@ module.exports = {
             ],
         }),
     ],
+    output: {
+        filename: 'js/bundle.js', // The main output bundle filename
+        clean: true,              // Clean the output directory
+    },
     optimization: {
         minimize: true,
         minimizer: [
